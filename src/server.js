a34231eb2913c6e66310d03fc34b0881c4916ee2
gokulakannan2705 <gokulakannanp003@gmail.com>
2026-02-20 16:32:58 +0530

    20-02-2026

--- src/server.js
+++ src/server.js
@@ -50,6 +50,7 @@ connectDB().then(async () => {
 // since the preview hostname changes per build we accept *.netlify.app
 const allowedOrigins = [
   "http://localhost:5173",
+  "http://localhost:5174",
   "http://localhost:3000",
   "https://testing-7i84.onrender.com",              // deployed backend (for sanity checks)
   "https://foxtaskmanager.netlify.app",
